68751655448847a74bb648221b76bca0dd2ee6a2
Bruno-CCosta <bruuuno.coosta@gmail.com>
2020-12-07 14:13:38 -0300

    cadastro/login

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { UserLogin } from '../model/UserLogin';
 import { Usuario } from '../model/Usuario';
 
@@ -9,11 +10,11 @@ import { Usuario } from '../model/Usuario';
 export class AuthService {
 
   constructor(private http: HttpClient) { }
-  logar(userLogin: UserLogin){
-    return this.http.post('http://localhost:8080/usuarios/logar', userLogin)
+  logar(userLogin: UserLogin) : Observable<UserLogin>{
+    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
   }
-  cadastrar(usuario:Usuario){
-    return this.http.post('http://localhost:8080/usuarios/cadastrar', usuario)
+  cadastrar(usuario: Usuario) : Observable<Usuario>{
+    return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
 
   }
 }
